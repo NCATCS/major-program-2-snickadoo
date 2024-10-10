56d381832feb202ee9eccbb05017416b873d2525
Kelvin Bryant <ksbryant1@gmail.com>
2024-10-10 03:13:31 -0400

    Create Student Class

---
+++ src/src/Student.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Student {
+    protected String major;
+    protected float gpa;
+    protected int creditHours;
+    protected ArrayList<String> classSchedule;
+
+    public Student() {}
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public float getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(float gpa) {
+        this.gpa = gpa;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+}
